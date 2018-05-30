4de5542e0d6b0aa21710bb8f23099c02f7e6db36
Jazib Sawar <jazibsawar@gmail.com>
2018-05-30 13:33:49 +0500

    404 & Generic Page Components

---
+++ components/404.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>404 Page Not Found</h1>
+    <div>Oops! The content you are looking for does not exist in your Cosmic JS Bucket.</div>
+  </div>
+</template>
---
+++ components/Page.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>{{ page.title }}</h1>
+    <div>{{ page.content}}</div>
+  </div>
+</template>
+<script>
+export default {
+   props: ['page'],
+}
+</script>
