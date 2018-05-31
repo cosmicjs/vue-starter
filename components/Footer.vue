39ff610608e1f0f1dc2e7084964b23a4ce28bf92
Tony Spiro <tspiro@tonyspiro.com>
2018-05-30 20:36:52 -0500

    Update Footer.vue

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
   <div className="footer">
-    &copy;{{ new Date().getFullYear() }} <a href="https://cosmicjs.com">Cosmic JS</a> Vue Starter
+    &copy;{{ new Date().getFullYear() }} <a href="https://cosmicjs.com">Cosmic JS</a> Vue Starter. <a href="https://github.com/cosmicjs/vue-starter" target="_blank">View the code</a>.
   </div>
 </template>
